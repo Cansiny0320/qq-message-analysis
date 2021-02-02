b1d128fbf29eab217e0459994e945585ce367d1f
Cansiny0320 <1136843910@qq.com>
2021-02-02 14:51:48 +0800

    feat: add some features

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const readline = require('readline')
+const fs = require("fs");
+const readline = require("readline");
 // const nodejieba = require('nodejieba')
 
 // async function read(file, encoding) {
@@ -10,45 +10,83 @@ const readline = require('readline')
 
 // read('./message.txt', 'utf-8')
 function read(file, callback) {
-  const fRead = fs.createReadStream(file)
+  const fRead = fs.createReadStream(file);
   const objReadline = readline.createInterface({
     input: fRead,
-  })
-  const message = {}
-  let isMessageLine = false
-  let pushKey = ''
-  let result = ''
-  objReadline.on('line', function (line) {
+  });
+  const message = {};
+  let isMessageLine = false;
+  let key = "";
+  const dateReg = /^\d{4}(-)(1[0-2]|0?\d)\1([0-2]\d|\d|30|31)$/;
+  objReadline.on("line", function (line) {
     if (isMessageLine) {
-      message[pushKey].push(line)
-      isMessageLine = false
+      message[key][message[key].length - 1].content = line;
+      isMessageLine = false;
     }
-    if (line.startsWith('2020') || line.startsWith('2021')) {
-      if (!message[line.split(' ')[0]]) {
-        message[line.split(' ')[0]] = []
+    if (dateReg.test(line.split(" ")[0]) && line.split(" ").length === 3) {
+      const [date, time, nickname] = line.split(" ");
+      if (!message[date]) {
+        message[date] = [
+          {
+            time,
+            nickname,
+          },
+        ];
+      } else {
+        message[date].push({
+          time,
+          nickname,
+        });
       }
-      pushKey = line.split(' ')[0]
-      isMessageLine = true
+      key = date;
+      isMessageLine = true;
     }
-  })
-  objReadline.on('close', function () {
-    callback(analyze(message))
-  })
+  });
+  objReadline.on("close", function () {
+    callback(analyze(message));
+  });
 }
 function analyze(message) {
-  const keys = Object.keys(message)
-  let maxDay = keys[0]
-  let total = 0
-  for (const key of keys) {
-    total += message[key].length
+  const keys = Object.keys(message);
+  let maxDay = keys[0];
+  let total = 0;
+  let firstTime = "";
+  let keyword = "mua";
+  let count = 0;
+  function mostDay(key) {
+    total += message[key].length;
     if (message[maxDay].length < message[key].length) {
-      maxDay = key
+      maxDay = key;
     }
   }
+  function search(key, keywordReg) {
+    const contentArr = message[key];
+    contentArr.forEach(item => {
+      const { content, time, nickname } = item;
+      const keywords = content.match(keywordReg);
+      if (keywords != null && count == 0) {
+        firstTime = `${key} ${time} ${nickname} : ${content}`;
+        count += keywords.length;
+      } else if (keywords != null) {
+        count += keywords.length;
+      }
+    });
+  }
+  for (const key of keys) {
+    mostDay(key);
+    search(key, new RegExp(keyword, "gi"));
+  }
   return `
-<p>你们已经聊了：${keys.length}天，总共${total}条消息</p>
+<h1>结果</h1>
+<p>你们第一次聊天是${keys[0]} ${message[keys[0]][0].time} ${message[keys[0]][0].nickname} : ${
+    message[keys[0]][0].content
+  }，到现在已经${Math.floor((+new Date() - +new Date(keys[0])) / (24 * 3600 * 1000))}天了</p>
+<p>你们一共聊了：${keys.length}天，总共${total}条消息，平均每天发${Math.round(
+    total / keys.length,
+  )}条消息</p>
 <p>你们聊天最多的一天是${maxDay}，一共发了${message[maxDay].length}条消息</p>
-  `
+<p>第一次说${keyword}是${firstTime},目前为止你们已经说过${keyword}${count}次</p>
+  `;
 }
 
-module.exports = read
+module.exports = read;
