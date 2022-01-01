76afd10ce32e07e0ef197a58178976b2002dd92d
Cansiny0320 <1136843910@qq.com>
2022-01-01 23:41:07 +0800

    feat: 静态资源服务器

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { getMessageMap, writeFile, resolve } from './utils'
 import { summaryOfYear } from './analysis'
+import express from 'express'
 async function main() {
   const messageMap = await getMessageMap(resolve('../message.txt'))
 
-  writeFile(resolve('../data.json'), summaryOfYear(messageMap, 2021))
+  writeFile(resolve('../public/data.json'), summaryOfYear(messageMap, 2021))
 }
 
 main()
+
+const app = express()
+app.use(express.static(resolve('../public')))
+app.listen(3000)
+
+console.log('访问 http://localhost:3000 查看报告')
